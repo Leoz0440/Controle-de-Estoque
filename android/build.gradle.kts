d87af1918e3bb103ed3cc204e729360f96e87386
Leoz0440 <leogamer0440@gmail.com>
2025-12-11 18:02:29 -0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -11,7 +11,3 @@ allprojects {
         mavenCentral()
     }
 }
-
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
