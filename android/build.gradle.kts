7934445a24887dc520dd561d6eae8cfe44fe279b
Leoz0440 <leogamer0440@gmail.com>
2025-12-11 17:07:32 -0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application") version "8.7.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.21" apply false // Use sua versão real
+    id("org.jetbrains.kotlin.android") version "1.8.22" apply false // Use sua versão real
     id("com.google.gms.google-services") version "4.4.4" apply false // <-- CORREÇÃO AQUI!
 }
 //...
