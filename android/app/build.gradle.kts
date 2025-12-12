6d539386352ed0b11cdef2a465a8e4676fc1625c
Leoz0440 <leogamer0440@gmail.com>
2025-12-12 11:13:37 -0300

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_1_8.toString()
+        jvmTarget = JavaVersion.VERSION_11.toString()
         freeCompilerArgs += listOf(
             "-Xallow-incompatible-mpp", 
             "-Xsuppress-version-string"
