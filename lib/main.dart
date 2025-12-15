ec6c48df2e7ed0a56e40f79425c7639a6bee65dd
Leoz0440 <leogamer0440@gmail.com>
2025-12-15 10:35:51 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,16 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   // Garante que a inicialização do Flutter está completa
   WidgetsFlutterBinding.ensureInitialized();
   // Inicializa o Firebase
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MDFStockApp());
 }
 
